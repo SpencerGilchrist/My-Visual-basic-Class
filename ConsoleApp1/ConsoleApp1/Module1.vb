da1a56bc4287679a3d00a538a2b2b3b66d3c8b7e
SpencerGilchrist <77285529+SpencerGilchrist@users.noreply.github.com>
2021-01-12 08:36:13 -0700

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Module1.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("    /\")
+        Console.WriteLine("   /  \")
+        Console.WriteLine("  /    \")
+        Console.WriteLine(" /      \")
+        Console.WriteLine("/________\")
+
+        Console.Beep(400, 500)
+
+
+        Console.ReadLine()
+
+    End Sub
+
+End Module
